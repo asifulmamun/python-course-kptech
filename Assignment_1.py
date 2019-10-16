ded136d54b72c3a65110419a7db22932047cd4ee
asifulmamun <asifulmamun@gmail.com>
2019-10-16 10:53:55 +0600

    just added file and comment

---
+++ Assignment_1.py
@@ -0,0 +1,11 @@
+"""
+---------------- KPtech Pyway---------------------
+	1. Comment “Single line” and “Multi line” and save it in “Assignment_1.py”
+	2. Save all in “Assignment_1.py” file.
+	3. Take two string input and convert them into integer, write comment before
+	each operations.
+	execute these operations:
+	+ , - , /, *, %
+	4. Take input as range. Check those numbers even or odd. Use while loop.
+	5. Print table of range 10. Use for loop
+"""
