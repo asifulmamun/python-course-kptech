f98e8add194e929bc1cb34ccc2e0a27dcf434f0d
asifulmamun <asifulmamun@gmail.com>
2020-01-14 19:27:00 +0600

    commented

--- Assignment_3.py
+++ Assignment_3.py
@@ -4,20 +4,20 @@
 """
 
 
-# 1. Input an Integer n and perform these tasks:
-n = int(input("Enter any integert Number for check Even or Odd: "))
+# # 1. Input an Integer n and perform these tasks:
+# n = int(input("Enter any integert Number for check Even or Odd: "))
 
-# Check Even or Odd, Queation-1.(i and ii)
-if n % 2 == 0:
-    print("Even\n")
-else:
-    print("Odd\n")
+# # Check Even or Odd, Queation-1.(i and ii)
+# if n % 2 == 0:
+#     print("Even\n")
+# else:
+#     print("Odd\n")
 
-# check Between 1-5 or not, Queation-1.(iii and iv)
-if n in range (1,5):
-    print("Firts Five\n")
-else:
-    print("Not in Five\n")
+# # check Between 1-5 or not, Queation-1.(iii and iv)
+# if n in range (1,5):
+#     print("Firts Five\n")
+# else:
+#     print("Not in Five\n")
 
 
 # 2. Make a list of Motorcycle with at least 4 data and perform these tasks:
@@ -28,13 +28,13 @@ motorCycleListAppended.append("Harley-Davidson") # Adding Value
 for motorCycleLists in motorCycleListAppended:
     print('\n', motorCycleLists)
 
-motorCycleListRemoved = motorCycleList
-motorCycleListRemoved.pop(-1) # remove last
-print(motorCycleListRemoved)
+# motorCycleListRemoved = motorCycleList
+# motorCycleListRemoved.pop(-1) # remove last
+# print(motorCycleListRemoved)
 
-motorCycleListReverse = motorCycleList
-motorCycleListReverse.reverse() # Reverse
-print(motorCycleListReverse)
+# motorCycleListReverse = motorCycleList
+# motorCycleListReverse.reverse() # Reverse
+# print(motorCycleListReverse)
 
-honda = motorCycleList[0:3]
-print(honda) # Why printed reverse 3
+# honda = motorCycleList[0:3]
+# print(honda) # Why printed reverse 3
